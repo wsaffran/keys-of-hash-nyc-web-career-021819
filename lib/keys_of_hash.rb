cda74cdc8a61c8d44dc854f771f6ae168bf05df0
Will Saffran <willh.saffran@gmail.com>
2019-02-14 21:45:45 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,11 +2,11 @@ class Hash
   
   def keys_of(arguments)
     animals = []
-		self.each { |animal, place|
+		self.each  do |animal, place|
 			if arguments.include?(place)
 				animals << animal
 			end
-		}
+		end
 		return animals
 	end
   
