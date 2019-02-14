2c24ae040f4a166535144df5d95739dccee40e0b
Will Saffran <willh.saffran@gmail.com>
2019-02-14 21:40:30 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,18 @@
 class Hash
+  
   def keys_of(arguments)
-    # code goes here
+    list_of_keys = []
+    if arguments == "Panama"
+      list_of_keys << "red-footed tortoise"
+    elsif arguments == "Madagascar"
+      list_of_keys << "aye-aye"
+      list_of_keys << "tomato frog"
+    elsif arguments == "Australia"
+      list_of_keys << "sugar glider"
+      list_of_keys << "kangaroo"
+      list_of_keys << "koala"
+    end
+    list_of_keys
   end
+  
 end
