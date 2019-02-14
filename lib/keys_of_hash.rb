d71ff472536b84f834eef50c3fd4737c82973c3d
Will Saffran <willh.saffran@gmail.com>
2019-02-14 21:44:59 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,18 +1,13 @@
 class Hash
   
   def keys_of(arguments)
-    list_of_keys = []
-    if arguments == "Panama"
-      list_of_keys << "red-footed tortoise"
-    elsif arguments == "Madagascar"
-      list_of_keys << "aye-aye"
-      list_of_keys << "tomato frog"
-    elsif arguments == "Australia"
-      list_of_keys << "sugar glider"
-      list_of_keys << "kangaroo"
-      list_of_keys << "koala"
-    end
-    list_of_keys
-  end
+    animals = []
+		self.each { |animal, place|
+			if arguments.include?(place)
+				animals << animal
+			end
+		}
+		return animals
+		end
   
 end
